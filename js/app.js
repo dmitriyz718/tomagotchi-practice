d6a477e16fdd54420e3f2f9928ea667b24a7c5d9
dmitriyzag <dzag17188@gmail.com>
2020-06-25 14:45:26 -0400

    constructor made w/ play, sleep feed and set and get health barebone structure

--- js/app.js
+++ js/app.js
@@ -1,2 +1,27 @@
 console.log(`Sanity Check`);
 /* document.querySelector(`#title h1`).innerText = (`Tomagotchi: Dima`); */
+// constructor
+class tomagotchi {
+    constructor(hunger = 5, boredom = 5, sleep = 5, health = 20, name = `Dima`) {
+        this.hunger = hunger;
+        this.boredom = boredom;
+        this.sleep = sleep;
+        this.health = health;
+        this.name = name;
+    }
+    play() {
+
+    };
+    putSleep() {
+
+    };
+    feed() {
+
+    };
+    set health(max = 5) {
+
+    };
+    get healthState() {
+
+    };
+}
